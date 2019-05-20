c1fb43758227dc7af5d11d9166baad017a948361
Paulo Suderio <paulo.suderio@gmail.com>
2019-05-20 03:04:05 +0000

    Creating site for 0.0.2

--- clover/treemap-dash-json.js
+++ clover/treemap-dash-json.js
@@ -1,4 +1,4 @@
-var treeMapJson = {"id":"Clover database Sun May 19 2019 21:50:51 BRT0","name":"","data":{
+var treeMapJson = {"id":"Clover database Sun May 19 2019 23:30:46 BRT0","name":"","data":{
     "$area":351.0,"$color":75.21368,"title":
     " 351 Elements, 75.2% Coverage"},"children":[{"id":"net.technearts0",
       "name":"net.technearts","data":{"$area":351.0,"$color":75.21368,
